34a672b2cb107e330cd5142102ff45e3f0452fd9
lundeen06 <lundeen.cahilly@gmail.com>
2025-10-08 18:25:21 -0700

    Clean rk4 integrator

--- src/integration/rk4.jl
+++ src/integration/rk4.jl
@@ -18,7 +18,11 @@ function rk4_step(f::Function, state::State, dt::Float64, params::Parameters)
     k2 = f(state + 0.5 * dt * k1, params)
     k3 = f(state + 0.5 * dt * k2, params)
     k4 = f(state + dt * k3, params)
-    
+
     new_state = state + (dt / 6.0) * (k1 + 2.0 * k2 + 2.0 * k3 + k4)
+
+    # Normalize quaternion to prevent numerical drift
+    normalize_quaternion!(new_state)
+
     return new_state
 end
