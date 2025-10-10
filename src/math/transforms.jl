1aec1a1d2687fb6fa819abb21fa777a74098b3ca
lundeen06 <lundeen.cahilly@gmail.com>
2025-10-10 11:08:41 -0700

    Add coordinate transforms

--- src/math/transforms.jl
+++ src/math/transforms.jl
@@ -1,6 +1,78 @@
 # Coordinate transformations and utilities
 
 using LinearAlgebra
+using SatelliteToolboxTransformations
+
+"""
+    eci_to_geocentric(r_eci)
+
+Convert ECI position vector to geocentric spherical coordinates.
+
+# Arguments
+- `r_eci::Vector{Float64}`: Position in ECI frame [m]
+
+# Returns
+- `(r, λ, Ω)`: Tuple of (radius [m], geocentric latitude [rad], longitude [rad])
+"""
+function eci_to_geocentric(r_eci::Vector{Float64})
+    x, y, z = r_eci
+
+    # Radius from Earth center
+    r = norm(r_eci)
+
+    # Geocentric latitude: angle from equatorial plane
+    λ = asin(z / r)
+
+    # Geocentric longitude: angle in equatorial plane from X-axis
+    Ω = atan(y, x)
+
+    return (r, λ, Ω)
+end
+
+"""
+    ned_to_eci(B_ned, λ, Ω, gmst)
+
+Transform vector from geocentric NED frame to ECI frame.
+
+# Arguments
+- `B_ned::Vector{Float64}`: Vector in NED frame (North, East, Down)
+- `λ::Float64`: Geocentric latitude [rad]
+- `Ω::Float64`: Geocentric longitude [rad]
+- `gmst::Float64`: Greenwich Mean Sidereal Time [rad]
+
+# Returns
+- `Vector{Float64}`: Vector in ECI frame
+"""
+function ned_to_eci(B_ned::Vector{Float64}, λ::Float64, Ω::Float64, gmst::Float64)
+    N, E, D = B_ned
+
+    # Rotation from NED to ECEF (geocentric)
+    # NED: X=North, Y=East, Z=Down
+    # ECEF: X through equator/prime meridian, Z through north pole
+    sin_λ = sin(λ)
+    cos_λ = cos(λ)
+    sin_Ω = sin(Ω)
+    cos_Ω = cos(Ω)
+
+    # Transform NED to ECEF
+    B_ecef = [
+        -sin_λ * cos_Ω * N - sin_Ω * E - cos_λ * cos_Ω * D,
+        -sin_λ * sin_Ω * N + cos_Ω * E - cos_λ * sin_Ω * D,
+         cos_λ * N - sin_λ * D
+    ]
+
+    # Rotation from ECEF to ECI (about Z-axis by GMST angle)
+    sin_gmst = sin(gmst)
+    cos_gmst = cos(gmst)
+
+    B_eci = [
+        cos_gmst * B_ecef[1] - sin_gmst * B_ecef[2],
+        sin_gmst * B_ecef[1] + cos_gmst * B_ecef[2],
+        B_ecef[3]
+    ]
+
+    return B_eci
+end
 
 """
     quat_to_dcm(q)
