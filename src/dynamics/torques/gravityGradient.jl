4e20f7e11b105b0fb5b4d3c938f90d080b57f372
Carson Lauer <lauercarson@gmail.com>
2025-10-23 20:46:40 -0700

    implemented gravity gradient - idk how to test it though

--- src/dynamics/torques/gravityGradient.jl
+++ src/dynamics/torques/gravityGradient.jl
@@ -2,7 +2,10 @@
 
 using LinearAlgebra
 
+
 using ..Satellite: Parameters, State
+
+using ..Constants: μ_earth
 """
     gravity_gradient_torque(state, params)
 
@@ -18,4 +21,13 @@ Compute gravity gradient torque on spacecraft.
 function gravity_gradient_torque(state::State, params::Parameters)
     # TODO: Implement gravity gradient torque
     # τ_gg = (3μ/r^5) * (r × I * r)
+
+    # I * r in body frame
+    r_b = quat_apply(state.q, state.r_eci)
+    r_p = quat_apply(params.principle_axes, r_b)
+    Ir_p =  params.principle_diag *. r_p
+    Ir_b = quat_apply(quat_inv(params.principle_axes), Ir_p)
+    
+    # Body frame torque
+    (constants.μ_earth/(norm(r_b)^5)) * cross(state.r_b, Ir_b)
 end
--- src/satellite/parameters.jl
+++ src/satellite/parameters.jl
@@ -8,12 +8,16 @@ Spacecraft physical parameters.
 # Fields
 - `mass::Float64`: Spacecraft mass [kg]
 - `inertia::Matrix{Float64}`: Inertia tensor [kg·m^2] (body frame)
+- `principle_diag::Vector{Float64}`: Inertia tensor diagonal elements 
+- `principle_axes::Quat`: Transformation from body frame to principle_axes frame - constant
 - `area::Float64`: Cross-sectional area [m^2]
 - `Cd::Float64`: Drag coefficient [-]
 """
 struct Parameters
     mass::Float64
     inertia::Matrix{Float64}  # 3x3 inertia tensor
+    principle_diag::Vector{Float64}
+    principle_axes::Quat
     area::Float64
     Cd::Float64
 end
