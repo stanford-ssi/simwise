369a469cd5381e632df2e45c4990f7ef919dd42b
Maya Davis <mkdavis721@gmail.com>
2025-10-30 20:11:51 -0700

    fixed small things in magnetometer

--- src/sensors/magnetometer.jl
+++ src/sensors/magnetometer.jl
@@ -6,49 +6,18 @@ using ..Satellite: State
 using ..Math: eci_to_body, Quat
 
 
-#Standard Deviation of Magnetometer [T]
-const σ = 1.5e-8 #value between 15 and 30 nT depending on the count number, using the largest number to be safe
+# Standard Deviation of Magnetometer [T]; from datasheet https://www.tri-m.com/products/pni/RM3100-User-Manual.pdf
+const σ = 1.5e-5   # value between 15 and 30 μT depending on the count number; using the largest number to be safe
 
-
-##Function transforms the velocity in ECI frame to body frame then adds noise
-function simulate_magnetometer(eci_magno::Vector{Float64}, quat_eci_to_body::Vector{Float64})
-    body_magno = eci_to_body(eci_magno, quat_eci_to_body)
-    return add_gauss(body_magno)
+# Function transforms the velocity in ECI frame to body frame, then adds noise
+function read_magnetometer(b_ref_eci::Vector{Float64}, quat_eci_to_body::Vector{Float64})
+    b_ref_body = eci_to_body(b_ref_eci, quat_eci_to_body)
+    return add_gauss(b_ref_body)
 end
 
-##Adds gaussian noise to gyro measurements
-    
+# Adds gaussian noise to gyro measurements
 function add_gauss(vec::Vector{Float64})
     noise = randn(length(vec)) * σ  # mean = 0, stddev = your value
     return vec .+ noise
 
 end
-
-
-#=
-
-
-# Example usage and plotting of magnetometer noise distribution
-# Need to have quat_state to test properly.
-
-using Plots
-
-true_gyro = [1.0, 1.0, 1.0]
-# Run many simulations
-N = 1000
-# results = [simulate_magnetometer(true_gyro, true_gyro) for _ in 1:N]  ##IMPORTANT: to use this, need quat state and working eci__to_body
-results = [add_gauss(true_gyro) for _ in 1:N]  # tests just the distrobution of the noise
-
-
-# Convert to matrix for plotting
-result_matrix = hcat(results...)  # 3 × N matrix: rows are x/y/z axes
-
-# Plot histograms for each axis
-plot(
-    histogram(result_matrix[1, :], bins=50, label="X-axis", title="Gyro Noise Distribution", xlabel="Value", ylabel="Count"),
-    histogram(result_matrix[2, :], bins=50, label="Y-axis"),
-    histogram(result_matrix[3, :], bins=50, label="Z-axis"),
-    layout = (3,1),
-    size=(600, 800)
-)
-=#
--- test/unit_tests/sensors/magnetometer.jl
+++ test/unit_tests/sensors/magnetometer.jl
@@ -5,27 +5,54 @@ using SatelliteToolboxTransformations
 
 using Simwise.Sensors: simulate_magnetometer
 
+# constants to define standard deviation
+const σ = 1.5e-5  # in μT
+const ε = 0.3e-5  # in μT
+
 @testset "Magnetometer Sensor Tests" begin
-
     # Test that noise is added correctly
-    true_magno = [1.0, 0.0, 0.0]  # True magnetic field in ECI frame
+    b_true_eci = [1.0, 0.0, 0.0]  # True magnetic field in ECI frame
     example_quat = [1.0, 0.0, 0.0, 0.0]  # Identity quaternion (no rotation)
-    measured_magno = simulate_magnetometer(true_magno, example_quat)
+    b_measured = simulate_magnetometer(b_true_eci, example_quat)
 
-    @test length(measured_magno) == 3
-    # Test that noise has expected standard deviation
+    @test length(b_measured) == 3
     N = 1000
-    measurements = [simulate_magnetometer(true_magno, example_quat) for _ in 1:N]
-    std_first = std([v[1] for v in measurements]) <= 1.8e-8 && std([v[1] for v in measurements]) >= 1.2e-8
-    std_second = std([v[2] for v in measurements]) <= 1.8e-8 && std([v[2] for v in measurements]) >= 1.2e-8
-    std_third = std([v[3] for v in measurements]) <= 1.8e-8 && std([v[3] for v in measurements]) >= 1.2e-8
+    measurements = [simulate_magnetometer(b_true_eci, example_quat) for _ in 1:N]
+
+    # Test that noise has expected standard deviation
+    std_first = std([v[1] for v in measurements]) <= (σ + ε) && std([v[1] for v in measurements]) >= (σ - ε)
+    std_second = std([v[2] for v in measurements]) <= (σ + ε) && std([v[2] for v in measurements]) >= (σ - ε)
+    std_third = std([v[3] for v in measurements]) <= (σ + ε) && std([v[3] for v in measurements]) >= (σ - ε)
 
     @test std_first
     @test std_second
     @test std_third
+end
 
-    
+@testset "Plotting measurements" begin
+    # Example usage and plotting of magnetometer noise distribution
+    # Need to have quat_state to test properly.
 
+    using Plots
+
+    true_gyro = [1.0, 1.0, 1.0]
+    # Run many simulations
+    N = 1000
+    # results = [simulate_magnetometer(true_gyro, true_gyro) for _ in 1:N]  
+    # IMPORTANT: to use this, need quat state and working eci_to_body
+    results = [add_gauss(true_gyro) for _ in 1:N]  # tests just the distribution of the noise
+
+    # Convert to matrix for plotting
+    result_matrix = hcat(results...)  # 3 × N matrix: rows are x/y/z axes
+
+    # Plot histograms for each axis
+    plot(
+        histogram(result_matrix[1, :], bins=50, label="X-axis", title="Gyro Noise Distribution", xlabel="Value", ylabel="Count"),
+        histogram(result_matrix[2, :], bins=50, label="Y-axis"),
+        histogram(result_matrix[3, :], bins=50, label="Z-axis"),
+        layout = (3,1),
+        size=(600, 800)
+    )
 end
 
 nothing
