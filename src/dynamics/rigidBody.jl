b73dc55e6a354a4845cd353a327475d624a70141
Colin Skinner <colinaskinner@gmail.com>
2025-11-06 15:47:39 -0800

    Testing works

--- src/dynamics/rigidBody.jl
+++ src/dynamics/rigidBody.jl
@@ -4,6 +4,7 @@ using LinearAlgebra
 
 using ..Satellite: Parameters, State
 using ..Constants: μ_earth
+using ..Math: Quat, quat_mult
 
 
 """
@@ -25,44 +26,26 @@ Compute attitude state derivatives (quaternion and angular velocity rates).
 - Quaternion kinematics: q_dot = 0.5 * Ω(ω) * q
 - Euler's equation: ω_dot = I^-1 * (τ - ω × (I * ω))
 """
-# function rigid_body_derivative(state::State, dt_s::Float64, force_eci::Vector{Float64}, torque_body::Vector{Float64})
 
-# end
+function rigid_body_derivative(t::Float64, state::Vector{Float64}, parameters::Parameters)
+    r = state[1:3]
+    v = state[4:6]
+    q = state[7:10]
+    ω = state[11:13]
+    # Position derivative is velocity 
+    r_dot = v
 
+    # Velocity derivative is acceleration (Schaub 2.15)
+    v_dot = parameters.force_eci / parameters.mass
 
-function rigid_body_derivative(t::Float64, state::Vector{Float64}, dt_s::Float64, force_eci::Vector{Float64}, torque_body::Vector{Float64})
+    # Quaternion derivative is based on hamilton product (Schaub 3.111)
+    q_dot = 0.5 * quat_mult(Quat(q), Quat(vcat(0, ω)))
 
+    # Angular derivative based on (Schaub 4.34-35)
+    I = parameters.inertia_body
+    I_inv = parameters.inertia_body_inv
+    τ = parameters.torque_body
+    ω_dot = I_inv * (τ - cross(ω, I * ω))
+
+    return vcat(r_dot, v_dot, q_dot, ω_dot)
 end
-
-"""
-
-    Vector3d r = state.segment<3>(0);
-    Vector3d v = state.segment<3>(3);
-    Quaterniond q(state(6), state(7), state(8), state(9));
-    Vector3d w = state.segment<3>(10);
-
-    VectorXd dot(13);
-
-    StateDerivative state_dot;
-
-    // Position derivative is velocity 
-    dot.segment<3>(0) = v;
-
-    // Velocity derivative is acceleration (Schaub 2.15)
-    dot.segment<3>(3) = this->force / this->mass_kg;
-
-    // Quaternion derivative is based on hamilton prodict (Schaub 3.109)
-    Quaterniond product = Cesium::Math::hamilton_product(this->state.q, this->state.w);
-    dot(6) = product.w();
-    dot(7) = product.x();
-    dot(8) = product.y();
-    dot(9) = product.z();
-
-    // Angular velocity derivative based on (Schaub 4.34)
-    // dwdt = I.inv * (τ - w x (Iw) )
-    dot.segment<3>(10) = this->InertiaTensor.inverse() * (this->torque - this->state.w.cross(this->InertiaTensor * this->state.w));
-
-    return dot;
-
-
-"""
--- test/unit_tests/dynamics/rigidBody.jl
+++ test/unit_tests/dynamics/rigidBody.jl
@@ -3,10 +3,33 @@ using LinearAlgebra
 
 using Simwise.Constants: μ_earth
 using Simwise.Dynamics: rigid_body_derivative
+#propagate_keplerian, state_from_oe
 
 
 @testset "Rigid Body Tests" begin
-    
+    @testset "Force No Torque" begin
+
+        q = Quat() # Identity
+        ω = [0,0,pi] # Rotating by π rad/s
+
+        # Initialized with (mass, inertia, force, torque) constructor
+        params = Parameters(
+            2.0, # kg
+            diagm([2.0, 2.0, 2.0]), # Inertia
+            [1.0,0.0,0.0], # x direction force
+            zeros(3) # no torque
+        )
+        
+        state = [
+            0.0, 0.0, 0.0, # r = 0
+            0.0, 0.0, 0.0, # v = 0
+            1.0, 0.0, 0.0, 0.0, # Identity quaternion
+            0.0, 0.0, 0.0 # ω = 0
+            ]
+
+        dot = rigid_body_derivative(0.0, state, params)
+
+    end
 end
 
 nothing
