456c59a3b69f1fcf25f57beac32d23ecca455c33
Colin Skinner <colinaskinner@gmail.com>
2025-11-06 14:39:26 -0800

    Small change to parameters

--- src/satellite/parameters.jl
+++ src/satellite/parameters.jl
@@ -10,10 +10,19 @@ Spacecraft physical parameters.
 - `inertia::Matrix{Float64}`: Inertia tensor [kg·m^2] (body frame)
 - `area::Float64`: Cross-sectional area [m^2]
 - `Cd::Float64`: Drag coefficient [-]
+- `force_eci::Vector{Float64}`: Total external forces [N·m] (ECI)
+- `torque_body::Vector{Float64}`:  Total external torques in [N·m] (body frame)
 """
-struct Parameters
+mutable struct Parameters
     mass::Float64
-    inertia::Matrix{Float64}  # 3x3 inertia tensor
+    inertia_body::Matrix{Float64}  # 3x3 inertia tensor
     area::Float64
     Cd::Float64
+    force_eci::Vector{Float64}
+    torque_body::Vector{Float64}
+end
+
+function Parameters(mass::Float64, inertia_body::Matrix{Float64}, area::Float64, Cd::Float64)
+    @assert length(inertia_body) == 9 "Inertia tensor must have 9 elements for a 3x3 matrix"
+    return Parameters(mass, inertia_body, area, Cd, zeros(3), zeros(3))
 end
