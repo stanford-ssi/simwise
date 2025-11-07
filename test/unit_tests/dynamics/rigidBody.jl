34e30254ab0a2f7b044c3338ec97926d01969a53
Colin Skinner <colinaskinner@gmail.com>
2025-11-06 17:20:55 -0800

    Random test works!

--- test/unit_tests/dynamics/rigidBody.jl
+++ test/unit_tests/dynamics/rigidBody.jl
@@ -9,9 +9,6 @@ using Simwise.Dynamics: rigid_body_derivative
 @testset "Rigid Body Tests" begin
     @testset "Force No Torque" begin
 
-        q = Quat() # Identity
-        ω = [0,0,pi] # Rotating by π rad/s
-
         # Initialized with (mass, inertia, force, torque) constructor
         params = Parameters(
             2.0, # kg
@@ -37,9 +34,6 @@ using Simwise.Dynamics: rigid_body_derivative
 
     @testset "Torque No Force" begin
 
-        q = Quat() # Identity
-        ω = [0,0,pi] # Rotating by π rad/s
-
         # Initialized with (mass, inertia, force, torque) constructor
         params = Parameters(
             2.0, # kg
@@ -62,6 +56,39 @@ using Simwise.Dynamics: rigid_body_derivative
         @test dot[7:10] == [0.0, 0.0, 0.0, 0.0]
         @test dot[11:13] == [0.5, 1.0, 2.0]
     end
+
+    @testset "Complete random nonsense testing" begin
+
+        I = [
+            15.0 0.5 0.5;
+            0.1 15.0 0.0;
+            0.0 3.0 4.0
+        ]
+
+        # Initialized with (mass, inertia, force, torque) constructor
+        params = Parameters(
+            17.0, # kg
+            I,
+            [12.0, 200.0, 14.0], # force
+            [0.4, 0.2, 0.7] # torque
+        )
+        
+        state = [
+            0.0, 0.0, 0.0, # r = 0
+            1.1, 1.2, 1.3, # v = 0
+            0.5, 0.5, 0.5, 0.5, # Identity quaternion
+            2.0, 3.14, 3.52 # ω = 0
+            ]
+
+        dot = rigid_body_derivative(0.0, state, params)
+
+        # Generated using Julia as a simple calculator with the textbook functions
+        @test isapprox(dot[1:3], [1.1, 1.2, 1.3], atol=1e-12)
+        @test isapprox(dot[4:6], [0.7058823529411765, 11.764705882352942, 0.8235294117647058], atol=1e-12)
+        @test isapprox(dot[7:10], [-2.165, 0.595, 0.40500000000000014, 1.165], atol=1e-12)
+        @test isapprox(dot[11:13], [6.1567301516750925, -4.715818201011166, 6.225913650758373], atol=1e-12)
+    end
+
 end
 
 nothing
